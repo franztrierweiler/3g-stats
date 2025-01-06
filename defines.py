7a7f4195b77b018ba7ac1ccc8e2876fd1f57846b
Franz <trierweilerfranz@gmail.com>
2025-01-06 18:53:18 +0100

    Ajout des defines

---
+++ defines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Projet 3g-stats
+# Fichier defines.py
+# Définitions des constantes globales
+
+# Chemins dans le répetoire d'exécution
+PATH_IN_CSV = "input"
+PATH_OUT_XLSX = "input"
+PATH_OUT_CONSOLIDATED_XLSX = "output"
+
+CSV_BASE_NAME = "BI-Suivi"
+
+if __name__ == '__main__':
+    print("Je suis un module")
--- xls_helper.py
+++ xls_helper.py
@@ -10,13 +10,12 @@ import csv
 from xlsxwriter.workbook import Workbook
 
 # Transforme un lot de fichiers CSV en fichiers XLS
-# in_csv: répertoire des fichiers CSV
-# out_xls: répertoire des fichiers XLSX
-def convert_csv_to_xlsx(in_csv, out_xls):
+# in_csv: répertoire des fichiers CSV où seront aussi écrits les fichiers XLSX
+def convert_csv_to_xlsx(in_csv):
     for csvfile in glob.glob(os.path.join(in_csv, '*.csv')):
-        workbook = Workbook(out_xls + csvfile[:-4] + '.xlsx')
+        workbook = Workbook(csvfile[:-4] + '.xlsx')
         worksheet = workbook.add_worksheet()
-        with open(csvfile, 'rt', encoding='utf8') as f:
+        with open(csvfile, 'rt') as f:
             reader = csv.reader(f)
             for r, row in enumerate(reader):
                 for c, col in enumerate(row):
